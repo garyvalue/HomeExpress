71c224a9c9b28793bcc164a8d5cece38660a13e1
JoeChow <76689555+JoeChowHoKeung@users.noreply.github.com>
2021-10-18 14:49:40 +0800

    Update telebot.py

--- telebot.py
+++ telebot.py
@@ -1,49 +1,64 @@
-import configparser
 import logging
 
-import telegram
-from flask import Flask, request
-from telgram.ext import Dispatcher, MessageHandler, Filters
-
-#Load data from config.ini file
-config = configparser.ConfigParser()
-config.read('config.ini')
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
+
 logger = logging.getLogger(__name__)
 
-# Initial Flask app
-app = Flask(__name__)
 
-# Initial bot by Telegram access token
-bot = telegram.Bot(token=(config['TELEGRAM']['ACCESS_TOKEN']))
-
-@app.route('/hook', methods=['POST'])
-def webhook_handler():
-    """Set route /hook with POST method will trigger this method."""
-    if request.method == "POST":
-        update = telegram.Update.de_json(request.get_json(force=True), bot)
-
-        # Update dispatcher process that handler to process this message
-        dispatcher.process_update(update)
-    return 'ok'
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
 
 
-def reply_handler(bot, update):
-    """Reply message."""
-    text = update.message.text
-    update.message.reply_text(text)
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
 
 
-# New a dispatcher for bot
-dispatcher = Dispatcher(bot, None)
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
 
-# Add handler for handling message, there are many kinds of message. For this handler, it particular handle text
-# message.
-dispatcher.add_handler(MessageHandler(Filters.text, reply_handler))
 
-if __name__ == "__main__":
-    # Running server
-    app.run(debug=True)
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater("TOKEN", use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
