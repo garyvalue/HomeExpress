9997f87b86d81087af51e7d40a007c70c1eb533d
JoeChow <76689555+JoeChowHoKeung@users.noreply.github.com>
2021-10-27 18:03:37 +0800

    Update main.py

--- refactoring/main.py
+++ refactoring/main.py
@@ -4,13 +4,18 @@ api_id = 8487685
 api_hash = 'b0e81650a39f65fc038b8a182581d428'
 # 2058616638:AAGOp7JqhzalJga69mP_7-vuOGvnJ9dOVZE
 
-import telegram
-import telegram.ext
-import re
-from random import randint
 import logging
-from telegram.ext import MessageHandler, Filters
-from telegram.ext import CommandHandler
+
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    ConversationHandler,
+    CallbackContext,
+)
+
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
@@ -23,40 +28,25 @@ dispatcher = updater.dispatcher
 updater.start_polling()
 updater.idle()
 
-def start(bot, update):
-    """
-    in the start command, will perform the following tasks
-    #1 : query the user_id to identify the sender
-        - if exists in database, 
-            @ seek the next command
-                ! /setting
-                ! /location
-                
-        - if not exists in database,
-            @ welcome letter
-            @ introduction
-            @ private policy
-            @ agreement
-            - if no, 
-                reply text, and reminder
-            - if yes, 
-                reply the inline button 
-                change the status of user
-                wait to receive the location
-    """
-    print(update)
 
-def location(bot, update):
-    """
-    in the location, will perform the following tasks
-    #1 : check the user_id status
-        if in setting mode,
-            ask for confirming the changes
-        if in execution mode,
-            matching
-    """
-    pass 
+class registeration:
+    def start(self, update: Update, context: CallbackContext) -> int:
+        pass
     
-dispatcher.add_handler(CommandHandler('start', start))
-
+    def private_policy(self):
+        pass
+        
+    def location_confirmation(self):
+        pass
+    
+    def completion(self):
+        pass
+    
+class user_setting:
+    def request(self) -> int:
+        pass
+    def confirmation(self) -> int:
+        pass
+    
+def location(self)
 # it will process all the command of /start than take an action 
